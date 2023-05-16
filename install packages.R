5d3a51fd50050c7b4134953df409e6c9713aa288
meriem1ayachi <128403173+meriem1ayachi@users.noreply.github.com>
2023-05-16 18:42:17 +0200

    Update and rename install.R to install packages.R

---
+++ install packages.R
@@ -0,0 +1,4 @@
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
