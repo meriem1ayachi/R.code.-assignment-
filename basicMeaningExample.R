1822933fdcdbc129d17cb0ab76b25a889277ed3c
meriem1ayachi <128403173+meriem1ayachi@users.noreply.github.com>
2023-05-16 18:37:21 +0200

    Text mining and word cloud fundamentals.R

--- basicMeaningExample.R
+++ basicMeaningExample.R
@@ -1,19 +1,56 @@
-#load table
-dataMeaning=read.table('meaningExample.txt', header=T, sep="\t")
-
-#plot with the labels
-plot(dataMeaning$length,dataMeaning$meanings,type="n",xlab="number of letters",ylab="number of meanings")
-text(dataMeaning$length,dataMeaning$meanings,dataMeaning$entry, cex= 1)
-
-#Now we'd like to build a regression model, i.e., find the equation for the straight line that best defines the dataset. 
-
-myModel = lm(dataMeaning$meanings~dataMeaning$length)
-summary(myModel)
-#least square fitting
-abline(myModel,col="red",lw="3")
-
-#quick and dirty trick to avoid label overlap (best solution: export to
-# vector format, like eps, and edit in separate software)
-newMeanings = jitter(dataMeaning$meanings, 5)
-plot(dataMeaning$length,newMeanings,type="n",xlab="number of letters",ylab="number of meanings")
-text(dataMeaning$length,newMeanings,dataMeaning$entry, cex= 1)
+#download packages.
+install.packages("tm")  
+install.packages("SnowballC") 
+install.packages("wordcloud") 
+install.packages("RColorBrewer") 
+#load packages 
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+#download the text from project gutenberg as doc.txt 
+#load the corpus 
+text <- readLines(file.choose())
+#load the data as a text
+docs <- Corpus(VectorSource(text))
+#inspect the content of the text
+inspect(docs)
+#text transformation: Replacing special characters with space
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word: choose the words you you don't want to include in the text mining.
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("talk", "look")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+docs <- tm_map(docs, stemDocument)
+#build a term document matrix 
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+#generate the word cloud
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=400, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+#Explore frequent terms and their associations
+findFreqTerms(dtm, lowfreq = 4)
+findAssocs(dtm, terms = "death", corlimit = 0.3)
+head(d, 10)
+#Plot word frequencies
+barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
+        col ="pink", main ="Most frequent words",
+        ylab = "Word frequencies")
